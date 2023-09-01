81e8a72378e2b4ada74376b416d83a463ed91f7d
Adrián Vicaria <adrianvicaria@gmail.com>
2023-09-01 23:35:27 +0200

    Fin 1a Jornada

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { UserInfo } from 'src/app/interfaces/user-info.interface';
+import { ProfilesService } from 'src/app/services/profiles.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
 
-  arrProfiles: UserInfo
+  arrProfiles: UserInfo[] = [];
+  //Inyecto el servicio
+  profilesService = inject(ProfilesService);
+  ngOnInit(): void {
+    //Llamo al servicio para obtener todos los perfiles
+    this.profilesService.getAll()
+    .then((response) =>{
+      this.arrProfiles = response.results;
+    }) //Resolve
+    .catch( (error) => {console.log("Error");
+    } ) //Reject
+  } 
 }
