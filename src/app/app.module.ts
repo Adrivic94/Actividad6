b564f1040175c2bca589cbced101bc09b9564eb4
Adrián Vicaria <adrianvicaria@gmail.com>
2023-09-01 18:48:54 +0200

    Creamos proyecto, componentes, pages y interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProfileCardComponent } from './components/profile-card/profile-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ViewProfileComponent } from './pages/view-profile/view-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    HeaderComponent,
+    ProfileCardComponent,
+    HomeComponent,
+    ViewProfileComponent
   ],
   imports: [
     BrowserModule,
